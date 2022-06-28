cb67418c695ab56e68633112780a61f0f0707329
Jiesi <61093868+jiesihu@users.noreply.github.com>
2022-06-28 19:18:08 +0800

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -1,9 +1,9 @@
-for runs_file in Jun26_10-06-31_hitlab-SYS-7048GR-TR
+for training_dir in Jun26_10-06-31_hitlab-SYS-7048GR-TR
 do
-    echo $runs_file
+    echo $training_dir
     Model_=CTE_Net
-    python evaluation.py --model_path ./$Model_/runs/$runs_file --GPU_id 2 --dataset val
-    python evaluation.py --model_path ./$Model_/runs/$runs_file --GPU_id 2 --dataset test
-    python Compute_Metric.py --output_path ./$Model_/runs/$runs_file/output
-    python Compute_Metric_test.py --output_path ./$Model_/runs/$runs_file/output
+    python evaluation.py --model_path ./$Model_/runs/$training_dir --GPU_id 2 --dataset val
+    python evaluation.py --model_path ./$Model_/runs/$training_dir --GPU_id 2 --dataset test
+    python Compute_Metric.py --output_path ./$Model_/runs/$training_dir/output
+    python Compute_Metric_test.py --output_path ./$Model_/runs/$training_dir/output
 done
